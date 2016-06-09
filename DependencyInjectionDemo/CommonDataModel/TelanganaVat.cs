c33bd657bcb9d987109620d1e8c7207891f17582
somubr <somnath.87@gmail.com>
2016-06-09 18:44:41 +0530

    Adding Demo of dependency injection

---
+++ DependencyInjectionDemo/CommonDataModel/TelanganaVat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonDataModel
+{
+    public class TelanganaVat : IVatCalculator
+    {
+
+        public decimal GetVatPercentage()
+        {
+            return 12;
+        }
+    }
+}
---
+++ DependencyInjectionDemo/DependencyInjectionDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using CommonDataModel;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace DependencyInjectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var container = new UnityContainer();
+            var section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
+            var containerName = Console.ReadLine();
+            IUnityContainer container = new UnityContainer().LoadConfiguration(section, containerName);
+
+
+            var vat = container.Resolve<IVatCalculator>();
+
+            Console.WriteLine(vat.GetVatPercentage());
+            Console.ReadLine();
+        }
+    }
+}
